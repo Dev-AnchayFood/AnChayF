0a912b436a1305082ead13117d1540ea62fd4742
NguyenHao <vonguyenhao08@gmail.com>
2024-12-26 15:40:29 +0700

    first commit

--- Desktop/CHO_CHAY/User app and web/lib/features/home/domain/services/advertisement_service_interface.dart
+++
@@ -1,5 +0,0 @@
-import 'package:sixam_mart/features/home/domain/models/advertisement_model.dart';
-
-abstract class AdvertisementServiceInterface {
-  Future<List<AdvertisementModel>?> getAdvertisementList();
-}
--- OneDrive/Documents/KTHDV_2023/nestjs-api-app/src/products/dto/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './insert.products.dto';
-export * from './update.products.dto';
